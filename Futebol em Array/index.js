8a324334a22ffdcddcb5d65f01687c23ded22793
Vini <vi058587@intelbras.com.br>
2023-05-30 15:23:09 -0300

    Atualizações no novo projeto

--- Futebol em Array/index.js
+++ Futebol em Array/index.js
@@ -1,7 +1,7 @@
-var goleiro, zagueiro1, zagueiro2, latdireito, latesquerdo, volante, meia1, meia2, pontaD, pontaE, centroAvante;
+var goleiro, zagueiro1, zagueiro3, zagueiro2, latdireito, latesquerdo, volante, alaE, alaD, meia3, meia1, meia2, pontaD, pontaE, atacante1, atacante2, centroAvante;
 var selecao = [];
 var timeElement = document.getElementById('time');
-
+var taticas 
 
 // Parâmetros para aceitar apenas letras (com acentos)
 function letras(input) {
@@ -21,27 +21,91 @@ function letras(input) {
 
 function campo() {
 
-    goleiro = prompt("Digite o nome do goleiro");
-    zagueiro1 = prompt("Digite o nome do primeiro zagueiro");
-    zagueiro2 = prompt("Digite o nome do segundo zagueiro");
-    latdireito = prompt("Digite o nome do lateral direito");
-    latesquerdo = prompt("Digite o nome do lateral esquerdo");
-    volante = prompt("Digite o nome do volante");
-    meia1 = prompt("Digite o nome do primeiro meia");
-    meia2 = prompt("Digite o nome do segundo meia");
-    pontaE = prompt("Digite o nome do ponta esquerda");
-    pontaD = prompt("Digite o nome do ponta direita");
-    centroAvante = prompt("Digite o nome do centroavante");
+    alert("Escolha a tática que deseja utilizar:")
+    taticas = prompt("A -> (4-3-3)\nB -> (3-5-2)\nC -> (4-4-2)\nD- > (4-5-1)\nSair")
+    
+    switch(taticas) {
+        case "A" || "a":
+        
+        goleiro = prompt("Digite o nome do goleiro");
+        zagueiro1 = prompt("Digite o nome do primeiro zagueiro");
+        zagueiro2 = prompt("Digite o nome do segundo zagueiro");
+        latdireito = prompt("Digite o nome do lateral direito");
+        latesquerdo = prompt("Digite o nome do lateral esquerdo");
+        volante = prompt("Digite o nome do volante");
+        meia1 = prompt("Digite o nome do primeiro meia");
+        meia2 = prompt("Digite o nome do segundo meia");
+        pontaE = prompt("Digite o nome do ponta esquerda");
+        pontaD = prompt("Digite o nome do ponta direita");
+        centroAvante = prompt("Digite o nome do centroavante");
+    if (!letras(goleiro) || !letras(zagueiro1) || !letras(zagueiro2) || !letras(latdireito) || !letras(latesquerdo) || !letras(volante) || !letras(meia1) || !letras(meia2) || !letras(pontaE) || !letras(pontaD) || !letras(centroAvante)) {
+        alert("Escalação inválida. Por favor, digite o nome do jogador (verifique de não digitar números).");
+        return;
+    }
+        selecao = [goleiro, '<br><br>', latdireito, zagueiro1, zagueiro2, latesquerdo, '<br><br>', volante, '<br>', meia1, meia2, '<br><br>', pontaD, , centroAvante, pontaE];
+        timeElement.innerHTML = selecao.join(' ');
+        break;
 
-    // Parâmetros sendo usados
+        case "B" || "b":
+        
+        goleiro = prompt("Digite o nome do goleiro");
+        zagueiro1 = prompt("Digite o nome do primeiro zagueiro");
+        zagueiro2 = prompt("Digite o nome do segundo zagueiro");
+        zagueiro3 = prompt("Digite o nome do terceiro zagueiro");
+        meia1 = prompt("Digite o nome do primeiro meia");
+        meia2 = prompt("Digite o nome do segundo meia");
+        meia3 = prompt("Digite o nome do teceiro meia");
+        alaE = prompt("Digite o nome do ala esquerda");
+        alaD = prompt("Digite o nome do ala direita");
+        atacante1 = prompt("Digite o nome do primeiro atacante");
+        atacante2 = prompt("Digite o nome do segundo atacante");
+        if (!letras(goleiro) || !letras(zagueiro1) || !letras(zagueiro2) || !letras(latdireito) || !letras(latesquerdo) || !letras(volante) || !letras(meia1) || !letras(meia2) || !letras(pontaE) || !letras(pontaD) || !letras(centroAvante)) {
+            alert("Escalação inválida. Por favor, digite o nome do jogador (verifique de não digitar números).");
+            return;
+        }
+            selecao = [goleiro, '<br><br>', zagueiro1, zagueiro2, zagueiro3,'<br><br>', meia1, meia2, meia3, '<br><br>', alaE, alaD,'<br><br>', atacante1, atacante2];
+            timeElement.innerHTML = selecao.join(' ');
+            break;
+
+        case "C" || "c":
+        
+        goleiro = prompt("Digite o nome do goleiro");
+        zagueiro1 = prompt("Digite o nome do primeiro zagueiro");
+        zagueiro2 = prompt("Digite o nome do segundo zagueiro");
+        latdireito = prompt("Digite o nome do lateral direito");
+        latesquerdo = prompt("Digite o nome do lateral esquerdo");
+        meia1 = prompt("Digite o nome do primeiro meia");
+        meia2 = prompt("Digite o nome do segundo meia");
+        alaE = prompt("Digite o nome do ala esquerda");
+        alaD = prompt("Digite o nome do ala direita");
+        atacante1 = prompt("Digite o nome do primeiro atacante");
+        atacante2 = prompt("Digite o nome do segundo atacante");
+        break;
+
+        case "D" || "d":
+        
+        goleiro = prompt("Digite o nome do goleiro");
+        zagueiro1 = prompt("Digite o nome do primeiro zagueiro");
+        zagueiro2 = prompt("Digite o nome do segundo zagueiro");
+        latdireito = prompt("Digite o nome do lateral direito");
+        latesquerdo = prompt("Digite o nome do lateral esquerdo");
+        volante = prompt("Digite o nome do volante");
+        meia1 = prompt("Digite o nome do primeiro meia");
+        meia2 = prompt("Digite o nome do segundo meia");
+        alaE = prompt("Digite o nome do ala esquerda");
+        alaD = prompt("Digite o nome do ala direita");
+        centroAvante = prompt("Digite o nome do centroavante");
+        break;
+
     if (!letras(goleiro) || !letras(zagueiro1) || !letras(zagueiro2) || !letras(latdireito) || !letras(latesquerdo) || !letras(volante) || !letras(meia1) || !letras(meia2) || !letras(pontaE) || !letras(pontaD) || !letras(centroAvante)) {
 
         alert("Escalação inválida. Por favor, digite o nome do jogador (verifique de não digitar números).");
         return;
     }
-
-    selecao = [goleiro, '<br><br>', latdireito, zagueiro1, zagueiro2, latesquerdo, '<br><br>', volante, '<br>', meia1, meia2, '<br><br>', pontaD, , centroAvante, pontaE];
-    timeElement.innerHTML = selecao.join(' ');
+        
+        case "Sair" || "sair":
+        return
+    }
 }
 
 function limpar() {
